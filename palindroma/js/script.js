11ff2bf74bb005b81649de654f49aa6761ba7a33
Cix8 <francesco.natal_fn@libero.it>
2022-04-21 15:52:25 +0200

    add 'isPalindorm(word)' function

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,1 +1,25 @@
-alert('ciao');
+const userInput = prompt('Scrivi una parola per verificare se è palindroma');
+let userResult = isPalindorm(userInput);
+
+
+/**
+ * Description
+ * @param {String} word -> is the word to check
+ * @returns {String} -> is the inverted word to check if the entered word is palindorm
+ */
+function isPalindorm(word) {
+    let result = '';
+
+    for (let i = userInput.length - 1; i >= 0; i--) {
+        result += userInput[i];
+        console.log(result); 
+    }
+    
+    if (result === userInput) {
+        console.log('La parola inserita è palindroma');
+    } else {
+        console.log('La parola inserita non è non palindroma');
+    }
+
+    return result
+}
